44b15cf76997170e6cf2af1a8e5447514dbcf1db
bharathmighty248 <bharathmighty248@gmail.com>
2021-07-30 08:31:20 +0530

    [Bharath] Add: Json Inventory Data Management Of Rice Wheat Pulse Implemented

---
+++ ObjectOrientedProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿using ObjectOrientedProgram.InventoryManagement;
+using System;
+
+namespace ObjectOrientedProgram
+{
+    class Program
+    {
+        const string INVENTORY_JSON =@"C:\Users\USER\source\repos\ObjectOrientedProgram\InventoryManagement\inventory.json";
+        static void Main(string[] args)
+        {
+            InventoryMain main = new InventoryMain();
+            main.DisplayData(INVENTORY_JSON);
+        }
+    }
+}
