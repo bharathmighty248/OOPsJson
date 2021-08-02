24ba3a05b1b1f3cdc4ec7d4d8d24ecadd45f8846
bharathmighty248 <bharathmighty248@gmail.com>
2021-08-02 08:01:16 +0530

    [Bharath] Inventory Management Methods are Implemented

--- ObjectOrientedProgram/InventoryManagement/InventoryMain.cs
+++ ObjectOrientedProgram/InventoryManagement/InventoryMain.cs
@@ -8,7 +8,7 @@ namespace ObjectOrientedProgram.InventoryManagement
 {
     public class InventoryMain
     {
-        public void ReadJsonFile(string filepath)
+        public void DisplayData(string filepath)
         {
             try
             {
@@ -16,22 +16,21 @@ namespace ObjectOrientedProgram.InventoryManagement
                 {
                     string jsonData = File.ReadAllText(filepath);
                     InventoryModel jsonObjectArray = JsonConvert.DeserializeObject<InventoryModel>(jsonData);
-
-                    Console.WriteLine("Name\tWeight\tRate\tAmount");
+                    Console.WriteLine("Type" + "\t" + "Name" + "\t" + "Weight" + "\t" + "Rate" + "\t" + "Amount");
                     List<Rice> rice = jsonObjectArray.RiceList;
                     foreach (var item in rice)
                     {
-                        Console.WriteLine("{0}" + "\t" + "{1}" + "\t" + "{2}" + "\t" + "{3}", item.Name, item.Weight, item.PricePerKg, item.Weight * item.PricePerKg);
+                        Console.WriteLine("Rice" + "\t" + "{0}" + "\t" + "{1}" + "\t" + "{2}" + "\t" + "{3}", item.Name, item.Weight, item.PricePerKg, item.Weight * item.PricePerKg);
                     }
                     List<Wheat> wheat = jsonObjectArray.WheatList;
                     foreach (var item in wheat)
                     {
-                        Console.WriteLine("{0}" + "\t" + "{1}" + "\t" + "{2}" + "\t" + "{3}", item.Name, item.Weight, item.PricePerKg, item.Weight * item.PricePerKg);
+                        Console.WriteLine("Wheat" + "\t" + "{0}" + "\t" + "{1}" + "\t" + "{2}" + "\t" + "{3}", item.Name, item.Weight, item.PricePerKg, item.Weight * item.PricePerKg);
                     }
                     List<Pulses> pulses = jsonObjectArray.PulsesList;
                     foreach (var item in pulses)
                     {
-                        Console.WriteLine("{0}" + "\t" + "{1}" + "\t" + "{2}" + "\t" + "{3}", item.Name, item.Weight, item.PricePerKg, item.Weight * item.PricePerKg);
+                        Console.WriteLine("Pulses" + "\t" + "{0}" + "\t" + "{1}" + "\t" + "{2}" + "\t" + "{3}", item.Name, item.Weight, item.PricePerKg, item.Weight * item.PricePerKg + "\n");
                     }
 
                 }
@@ -45,6 +44,229 @@ namespace ObjectOrientedProgram.InventoryManagement
                 throw new Exception(e.Message);
             }
             
+        }
+        public void Edit(String filepath)
+        {
+            string jsonData = File.ReadAllText(filepath);
+            InventoryModel jsonObjectArray = JsonConvert.DeserializeObject<InventoryModel>(jsonData);
+            String Type = "";
+            String Check = "";
+            Console.WriteLine("Enter a List name(Rice,Wheat or Pulses) to edit :");
+            Check = Console.ReadLine().ToLower();
+            switch (Check)
+            {
+                case "rice":
+                    Console.WriteLine("Enter rice type to edit:");
+                    Type = Console.ReadLine().ToLower();
+                    List<Rice> rice = jsonObjectArray.RiceList;
+                    foreach (var item in rice)
+                    {
+                        if (item.Name.ToLower() == Type)
+                        {
+                            Console.WriteLine("Enter new rice Weight :");
+                            item.Weight = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter new rice PricePerKg :");
+                            item.PricePerKg = Convert.ToInt32(Console.ReadLine());
+                        }
+                        else
+                        {
+                            Console.WriteLine("Name doesnt Exist!");
+                        }
+
+                    }
+                    InventoryMain.WriteToFile(jsonObjectArray);
+                    Console.WriteLine("DATA EDITED!");
+                    break;
+
+                case "wheat":
+                    Console.WriteLine("Enter wheat type to edit:");
+                    Type = Console.ReadLine().ToLower();
+                    List<Wheat> wheat = jsonObjectArray.WheatList;
+                    foreach (var item in wheat)
+                    {
+                        if (item.Name.ToLower() == Type)
+                        {
+                            Console.WriteLine("Enter new wheat Weight :");
+                            item.Weight = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter new wheat PricePerKg :");
+                            item.PricePerKg = Convert.ToInt32(Console.ReadLine());
+                        }
+                        else
+                        {
+                            Console.WriteLine("Name doesnt Exist!");
+                        }
+                    }
+                    InventoryMain.WriteToFile(jsonObjectArray);
+                    Console.WriteLine("DATA EDITED!");
+                    break;
+
+                case "pulses":
+                    Console.WriteLine("Enter rice type to edit:");
+                    Type = Console.ReadLine().ToLower();
+                    List<Pulses> pulses = jsonObjectArray.PulsesList;
+                    foreach (var item in pulses)
+                    {
+                        if (item.Name.ToLower() == Type)
+                        {
+                            Console.WriteLine("Enter new pulses Weight :");
+                            item.Weight = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Enter new pulses PricePerKg :");
+                            item.PricePerKg = Convert.ToInt32(Console.ReadLine());
+                        }
+                        else
+                        {
+                            Console.WriteLine("Name doesnt Exist!");
+                        }
+                    }
+                    InventoryMain.WriteToFile(jsonObjectArray);
+                    Console.WriteLine("DATA EDITED!");
+                    break;
+
+                default:
+                    Console.WriteLine("Name doesnt exist!");
+                    break;
+            }
+        }
+        public static void WriteToFile(InventoryModel inventory)
+        {
+            string jsonConvrsion = JsonConvert.SerializeObject(inventory);
+            System.IO.File.WriteAllText(@"D:\BridgeLabz Problems Git Hub Local Repository\OOPsJson\ObjectOrientedProgram\InventoryManagement\inventory.json", jsonConvrsion);
+        }
+        public void Deleteitems(String filepath)
+        {
+            string jsonData = File.ReadAllText(filepath);
+            InventoryModel jsonObjectArray = JsonConvert.DeserializeObject<InventoryModel>(jsonData);
+            String Type = "";
+            int counter = 0;
+            String Check = "";
+            Console.WriteLine("Enter a List name(Rice,Wheat or Pulses) to edit :");
+            Check = Console.ReadLine().ToLower();
+            switch (Check)
+            {
+                case "rice":
+                    Console.WriteLine("Enter rice type to delete:");
+                    Type = Console.ReadLine().ToLower();
+                    List<Rice> rice = jsonObjectArray.RiceList;
+                    foreach (var item in rice.ToArray())
+                    {
+                        counter++;
+                        if (item.Name.ToLower() == Type)
+                        {
+                            rice.Remove(item);
+                        }
+                        else
+                        {
+                            if (counter == rice.Count)
+                                Console.WriteLine("Name doesnt Exist!");
+                        }
+                    }
+                    InventoryMain.WriteToFile(jsonObjectArray);
+                    Console.WriteLine("DATA DELTED!");
+                    break;
+
+
+                case "wheat":
+                    Console.WriteLine("Enter wheat type to Delete:");
+                    Type = Console.ReadLine().ToLower();
+                    List<Wheat> wheat = jsonObjectArray.WheatList;
+                    foreach (var item in wheat.ToArray())
+                    {
+                        if (item.Name.ToLower() == Type)
+                        {
+                            wheat.Remove(item);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Name doesnt Exist!");
+                        }
+                    }
+                    InventoryMain.WriteToFile(jsonObjectArray);
+                    Console.WriteLine("DATA DELTED!");
+                    break;
+
+                case "pulses":
+                    Console.WriteLine("Enter pulses type to Delete:");
+                    Type = Console.ReadLine().ToLower();
+                    List<Pulses> pulses = jsonObjectArray.PulsesList;
+                    foreach (var item in pulses.ToArray())
+                    {
+                        if (item.Name.ToLower() == Type)
+                        {
+                            if (item.Name.ToLower() == Type)
+                            {
+                                pulses.Remove(item);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Name doesnt Exist!");
+                            }
+                        }
+                    }
+                    InventoryMain.WriteToFile(jsonObjectArray);
+                    Console.WriteLine("DATA DELTED!");
+                    break;
+                default:
+                    Console.WriteLine("Name doesnt exist!");
+                    break;
+            }
+
+        }
+        public void Additems(String filepath)
+        {
+            string jsonData = File.ReadAllText(filepath);
+            InventoryModel jsonObjectArray = JsonConvert.DeserializeObject<InventoryModel>(jsonData);
+            String Check = "";
+            Console.WriteLine("Enter a List name(Rice,Wheat or Pulses) to edit :");
+            Check = Console.ReadLine().ToLower();
+            switch (Check)
+            {
+                case "rice":
+                    List<Rice> rice = jsonObjectArray.RiceList;
+                    Rice rc = new Rice();
+                    Console.WriteLine("Enter new rice type :");
+                    rc.Name = Console.ReadLine().ToLower();
+                    Console.WriteLine("Enter Weight of new rice :");
+                    rc.Weight = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter PricePerKg of new Rice :");
+                    rc.PricePerKg = Convert.ToInt32(Console.ReadLine());
+                    rice.Add(rc);
+                    InventoryMain.WriteToFile(jsonObjectArray);
+                    Console.WriteLine("DATA ADDED!");
+                    break;
+
+
+                case "wheat":
+
+                    List<Wheat> wheat = jsonObjectArray.WheatList;
+                    Wheat wh = new Wheat();
+                    Console.WriteLine("Enter new wheat type :");
+                    wh.Name = Console.ReadLine().ToLower();
+                    Console.WriteLine("Enter Weight of new rice :");
+                    wh.Weight = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter PricePerKg of new Wheat :");
+                    wh.PricePerKg = Convert.ToInt32(Console.ReadLine());
+                    wheat.Add(wh);
+                    InventoryMain.WriteToFile(jsonObjectArray);
+                    Console.WriteLine("DATA ADDED!");
+                    break;
+
+                case "pulses":
+                    List<Pulses> pulses = jsonObjectArray.PulsesList;
+                    Pulses pl = new Pulses();
+                    Console.WriteLine("Enter new pulses type :");
+                    pl.Name = Console.ReadLine().ToLower();
+                    Console.WriteLine("Enter Weight of new pulses :");
+                    pl.Weight = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter PricePerKg of new Pulses :");
+                    pl.PricePerKg = Convert.ToInt32(Console.ReadLine());
+                    pulses.Add(pl);
+                    InventoryMain.WriteToFile(jsonObjectArray);
+                    Console.WriteLine("DATA ADDED!");
+                    break;
+                default:
+                    Console.WriteLine("Name doesnt exist!");
+                    break;
+            }
         }
     }
 }
--- ObjectOrientedProgram/Program.cs
+++ ObjectOrientedProgram/Program.cs
@@ -8,8 +8,32 @@ namespace ObjectOrientedProgram
         const string INVENTORY_JSON = @"D:\BridgeLabz Problems Git Hub Local Repository\OOPsJson\ObjectOrientedProgram\InventoryManagement\inventory.json";
         static void Main(string[] args)
         {
+            string choice="start";
             InventoryMain main = new InventoryMain();
-            main.ReadJsonFile(INVENTORY_JSON);
+            Console.WriteLine("\nWelCome To Json Inventory Management\nSelect one Option below..\n1. Add\n2.Edit\n3. Delete\n4. Display\n5. Stop");
+            while (choice != "5")
+            {
+                choice = Console.ReadLine().ToLower();
+                switch (choice)
+                {
+                    case "1":
+                        main.Additems(INVENTORY_JSON);
+                        break;
+
+                    case "2":
+                        main.Edit(INVENTORY_JSON);
+                        break;
+
+                    case "3":
+                        main.Deleteitems(INVENTORY_JSON);
+                        break;
+
+                    case "4":
+                        main.DisplayData(INVENTORY_JSON);
+                        break;
+
+                }
+            }
         }
     }
 }
